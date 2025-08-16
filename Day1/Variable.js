89c45614ff108338d1b7043b9f1e673ba165d5ba
revathi22-1 <revathi22590@gmail.com>
2025-08-16 22:12:06 +0200

    Add files via upload
    Day 1- Variables

---
+++ Day1/Variable.js
@@ -0,0 +1,27 @@
+var firstName = "Revathi"
+console.log(typeof firstName)
+var CompanyName ="ABB"
+console.log(typeof CompanyName)
+var mobileNumber = 9898989898
+console.log(typeof mobileNumber)
+var isAutomation = "Yes"
+console.log(typeof isAutomation)
+var hasPlaywright
+console.log(typeof hasPlaywright)
+var transactionId= null
+console.log(typeof transactionId)
+
+
+//intentionallly the value is not given to the Object it will return object while executing
+//like hasPlaywright no value is assigned, it returns undefined ..so in future they may use the 
+//object to assign value
+//var mobilenumber = 84758475947934937n  // bigInt (long numbers)
+//var id=Symbol() unique information 
+//var let const  ES15
+//var allows redeclaration and reassignment of variables
+//var un="t"
+//var un="r" ( last assignment will be consider as output)
+//let allows to reassign but cannot redeclaration.
+//Javascript allows let to reassign string and also reassign int
+//ex : let num = 1
+//     num ="Test"  
